731eb79125de549c5fb1581692939b6b91df0c05
Allen_Lin_ <1160208849wfl@gmail.com>
2018-03-14 22:16:46 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyDMAZY3sTpQXY6_ljSmwInxwj35_H7muhQ');//这里改成您的api key
+define('APIKEY', '0000');//这里改成您的api key
 define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', 'Heroku-水管');//网站名字
 define('TITLENAME', 'Heroku-水管');//网站名字
-define('EN2DEKEY', 'lo3v-}[a0nf\');//一个随机字符串，加解密KEY
-define('EMAIL', 'wufl67@yandex.com');//邮箱
+define('EN2DEKEY', '0000');//一个随机字符串，加解密KEY
+define('EMAIL', '0000');//邮箱
 ?>
